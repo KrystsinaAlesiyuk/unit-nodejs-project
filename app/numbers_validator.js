3af72ebe9effb16bd7490e4122be26c5a99160fa
Hleb_Papshau <hleb_papshau@epam.com>
2021-07-28 19:37:07 +0300

    Fix eslint and typos

--- app/numbers_validator.js
+++ app/numbers_validator.js
@@ -1,33 +1,29 @@
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable padded-blocks */
-/* eslint-disable max-len */
-/* eslint-disable space-before-blocks */
 /**
  *
  * A simple class containing methods for validating numbers
  * @class NumbersValidator
  */
 class NumbersValidator {
-    /**
-     * Creates an instance of NumbersValidator.
-     * @memberof NumbersValidator
-     */
-    constructor() {
+  /**
+   * Creates an instance of NumbersValidator.
+   * @memberof NumbersValidator
+   */
+  constructor() {
 
-    };
-        /**
-         *
-         *
-         * @param {Number} number number to check
-         * @return {Boolean} true if number is even
-         * @memberof NumbersValidator
-         */
-    isNumberEven(number){
-     const typeOfVariable = typeof number;
-     if (typeOfVariablen !== 'number') {
+  };
+  /**
+   *
+   *
+   * @param {Number} number number to check
+   * @return {Boolean} true if number is even
+   * @memberof NumbersValidator
+   */
+  isNumberEven(number) {
+    const typeOfVariable = typeof number;
+    if (typeOfVariable !== 'number') {
       throw new Error(`[${number}] is not of type "Number" it is of type "${typeOfVariable}"`);
-     } else {
-       return number % 2 === 0;
+    } else {
+      return number % 2 === 0;
     }
   };
 
@@ -39,15 +35,14 @@ class NumbersValidator {
    * @memberof NumbersValidator
    */
   getEvenNumbersFromArray(arrayOfNumbers) {
-      if (Array.isArray(arrayOfNumbers) &&
-      arrayOfNumbers.every((item) => typeof item === 'number')){
+    if (Array.isArray(arrayOfNumbers) &&
+      arrayOfNumbers.every((item) => typeof item === 'number')) {
       const arrayOfEvenNumbers = arrayOfNumbers.filter(this.isNumberEven);
       return arrayOfEvenNumbers;
-  } else {
-      throw new Error(`[${arrayOfNumbers}] is not ah array of "Numbers"`)
-
+    } else {
+      throw new Error(`[${arrayOfNumbers}] is not ah array of "Numbers"`);
     }
- }
-}; 
+  }
+};
 
 module.exports = NumbersValidator;
--- test/specs/isNumberEven.spec.js
+++ test/specs/isNumberEven.spec.js
@@ -1,15 +1,16 @@
-const NumbersValidator = '../../app/numbers_validator'
-const {expect} = 'chai';
+const NumbersValidator = require('../../app/numbers_validator.js');
+const {expect} = require('chai');
 
-describe(`isNumberEven positive scenarios`, function(){
-    let validator;
-    beforeEach(function(){
-        vaidator = new NumbersValidator();
+describe(`isNumberEven positive scenarios`, function() {
+  let validator;
+  beforeEach(function() {
+    validator = new NumbersValidator();
   });
-    afterEach(function(){
-      validator = null;
+  afterEach(function() {
+    validator = null;
   });
+
   it(`should return true if number is even`, function() {
-      expect(validator.isNumberEven(4)).to.be.equal(true);
+    expect(validator.isNumberEven(4)).to.be.equal(true);
   });
 });
